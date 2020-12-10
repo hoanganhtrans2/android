466c7b1c79476cc59bbb6865d97170019f4a9079
hoanganhtrans2 <hoanganhtrans23@gmail.com>
2020-12-10 21:33:08 +0700

    hiển thị tên danh sách người dùng

--- app/src/main/java/com/example/onthi/MainActivity.java
+++ app/src/main/java/com/example/onthi/MainActivity.java
@@ -5,6 +5,18 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -13,20 +25,41 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView rcvName;
     private  NameListAdapter nameListAdapter;
     private ArrayList<String> nameArrayList;
+    String url = "https://5fd066b11f2374001663174d.mockapi.io/api/usertable";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //ghi chu
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         rcvName = findViewById(R.id.rcvName);
         nameArrayList = new ArrayList<>();
-        nameArrayList.add("hoang anh");
-        nameArrayList.add("hoang anh");
-        nameArrayList.add("hoang anh");
-        nameArrayList.add("hoang anh");
+        GetArrayJson(url);
         nameListAdapter = new NameListAdapter(this, nameArrayList);
 
-        rcvName.setAdapter(nameListAdapter);
+
         rcvName.setLayoutManager(new LinearLayoutManager(this));
+    }
+    private void GetArrayJson(String url){
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                for(int i=0; i<response.length(); i++){
+                    try {
+                        JSONObject object = (JSONObject) response.get(i);
+                        nameArrayList.add(object.getString("name").toString());
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                rcvName.setAdapter(nameListAdapter);
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, "Error by get Json Array!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(jsonArrayRequest);
     }
 }
